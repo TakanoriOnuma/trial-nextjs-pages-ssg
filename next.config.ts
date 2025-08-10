85be242d0602cd9132fa5e8e3d7796c31ce6306e
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-10 18:06:34 +0900

    Next.jsをSSGした

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   trailingSlash: true,
-  /* config options here */
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
