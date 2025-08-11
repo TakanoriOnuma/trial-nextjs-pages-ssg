b5a0c8b963a499f3dd6e27c8824785c7c5199935
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-12 01:37:18 +0900

    自前の404ハンドリングをコメントアウトしてみる

--- worker.js
+++ worker.js
@@ -3,24 +3,24 @@ export default {
     // デフォルトは静的アセット返す
     const res = await env.ASSETS.fetch(request);
 
-    // リクエストされたURLが存在しない場合は404ページを返す
-    if (res.status === 404) {
-      // 404.html を Assets から取得
-      const notFoundRes = await env.ASSETS.fetch(
-        new Request(new URL("/404.html", request.url), request)
-      );
+    // // リクエストされたURLが存在しない場合は404ページを返す
+    // if (res.status === 404) {
+    //   // 404.html を Assets から取得
+    //   const notFoundRes = await env.ASSETS.fetch(
+    //     new Request(new URL("/404.html", request.url), request)
+    //   );
 
-      // 404.html があればそれを返す
-      if (notFoundRes.ok) {
-        return new Response(notFoundRes.body, {
-          status: 404,
-          headers: notFoundRes.headers,
-        });
-      }
+    //   // 404.html があればそれを返す
+    //   if (notFoundRes.ok) {
+    //     return new Response(notFoundRes.body, {
+    //       status: 404,
+    //       headers: notFoundRes.headers,
+    //     });
+    //   }
 
-      // 404.html も存在しない場合はシンプルな404メッセージ
-      return new Response("404 Not Found", { status: 404 });
-    }
+    //   // 404.html も存在しない場合はシンプルな404メッセージ
+    //   return new Response("404 Not Found", { status: 404 });
+    // }
 
     return res;
   },
