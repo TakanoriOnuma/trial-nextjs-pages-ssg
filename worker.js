5d42b36bdb05643b9da2f2a89ccc80d0109327ee
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-12 01:08:46 +0900

    awaitを書き忘れていたので書いた

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
     // デフォルトは静的アセット返す
-    const res = env.ASSETS.fetch(request);
+    const res = await env.ASSETS.fetch(request);
 
     // リクエストされたURLが存在しない場合は404ページを返す
     if (res.status === 404) {
