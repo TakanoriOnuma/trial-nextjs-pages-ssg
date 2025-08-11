eeb69d0e0089329a8b8590f6c17ce22a175eb374
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-12 01:19:55 +0900

    404ページを丁寧に返す

--- worker.js
+++ worker.js
@@ -5,7 +5,21 @@ export default {
 
     // リクエストされたURLが存在しない場合は404ページを返す
     if (res.status === 404) {
-      return new Response(new URL("/404.html", request.url), request);
+      // 404.html を Assets から取得
+      const notFoundRes = await env.ASSETS.fetch(
+        new Request(new URL("/404.html", request.url), request)
+      );
+
+      // 404.html があればそれを返す
+      if (notFoundRes.ok) {
+        return new Response(notFoundRes.body, {
+          status: 404,
+          headers: notFoundRes.headers,
+        });
+      }
+
+      // 404.html も存在しない場合はシンプルな404メッセージ
+      return new Response("404 Not Found", { status: 404 });
     }
 
     return res;
