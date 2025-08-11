70113c886e9f1f9f48f7697a8056aa92f6b9e121
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-12 01:19:21 +0900

    Revert "_redirectsで404ページを返せるか試す"
    This reverts commit 5cf45fb8a316c58551c1708469b8e420351d21c3.

--- worker.js
+++ worker.js
@@ -4,23 +4,9 @@ export default {
     const res = await env.ASSETS.fetch(request);
 
     // リクエストされたURLが存在しない場合は404ページを返す
-    // if (res.status === 404) {
-    //   // 404.html を Assets から取得
-    //   const notFoundRes = await env.ASSETS.fetch(
-    //     new Request(new URL("/404.html", request.url), request)
-    //   );
-
-    //   // 404.html があればそれを返す
-    //   if (notFoundRes.ok) {
-    //     return new Response(notFoundRes.body, {
-    //       status: 404,
-    //       headers: notFoundRes.headers,
-    //     });
-    //   }
-
-    //   // 404.html も存在しない場合はシンプルな404メッセージ
-    //   return new Response("404 Not Found", { status: 404 });
-    // }
+    if (res.status === 404) {
+      return new Response(new URL("/404.html", request.url), request);
+    }
 
     return res;
   },
