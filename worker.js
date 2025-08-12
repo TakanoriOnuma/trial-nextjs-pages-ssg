ce467668e44ab938e294fac6ddfbeaf3f33e0da4
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-12 09:53:18 +0900

    not_found_handlingも自前で定義してみる

--- worker.js
+++ worker.js
@@ -19,23 +19,23 @@ export default {
     const res = await env.ASSETS.fetch(request);
 
     // // リクエストされたURLが存在しない場合は404ページを返す
-    // if (res.status === 404) {
-    //   // 404.html を Assets から取得
-    //   const notFoundRes = await env.ASSETS.fetch(
-    //     new Request(new URL("/404.html", request.url), request)
-    //   );
+    if (res.status === 404) {
+      // 404.html を Assets から取得
+      const notFoundRes = await env.ASSETS.fetch(
+        new Request(new URL("/404.html", request.url), request)
+      );
 
-    //   // 404.html があればそれを返す
-    //   if (notFoundRes.ok) {
-    //     return new Response(notFoundRes.body, {
-    //       status: 404,
-    //       headers: notFoundRes.headers,
-    //     });
-    //   }
+      // 404.html があればそれを返す
+      if (notFoundRes.ok) {
+        return new Response(notFoundRes.body, {
+          status: 404,
+          headers: notFoundRes.headers,
+        });
+      }
 
-    //   // 404.html も存在しない場合はシンプルな404メッセージ
-    //   return new Response("404 Not Found", { status: 404 });
-    // }
+      // 404.html も存在しない場合はシンプルな404メッセージ
+      return new Response("404 Not Found", { status: 404 });
+    }
 
     return res;
   },
