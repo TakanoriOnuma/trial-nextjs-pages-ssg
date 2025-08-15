47cbb090c4623804e64d2208d822655b09b5c731
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-15 12:44:41 +0900

    細かい微調整

--- worker.ts
+++ worker.ts
@@ -3,9 +3,7 @@ import { WorkerEntrypoint } from "cloudflare:workers";
 class Worker extends WorkerEntrypoint<Env> {
   async fetch(request: Request) {
     // リライトの設定
-    const url = new URL(request.url);
-    const match = url.pathname.match(/^\/dynamic\/([^/]+)\/?$/);
-
+    const match = request.url.match(/^\/dynamic\/([^/]+)\/?$/);
     if (match) {
       const assetUrl = new URL("/dynamic/[id]/index.html", request.url);
       return this.env.ASSETS.fetch(assetUrl.toString());
