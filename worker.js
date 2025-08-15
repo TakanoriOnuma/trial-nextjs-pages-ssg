c56bea2fd32768554d2f54fcff37125031555710
大沼 峻徳 <numa.san.1024@gmail.com>
2025-08-15 10:34:32 +0900

    env.ASSETSを使わないで単純にfetchで取得してみる

--- worker.js
+++ worker.js
@@ -6,39 +6,40 @@ export default {
 
     if (match) {
       url.pathname = `/dynamic/[id]/index.html`;
-      const assetRes = await env.ASSETS.fetch(new Request(url, request));
+      return fetch(new Request(url, request));
+      // const assetRes = await env.ASSETS.fetch(new Request(url, request));
 
-      const headers = new Headers(assetRes.headers);
-      headers.delete("Location"); // リダイレクトヘッダーを削除
+      // const headers = new Headers(assetRes.headers);
+      // headers.delete("Location"); // リダイレクトヘッダーを削除
 
-      // ヘッダーとボディだけ返す
-      return new Response(assetRes.body, {
-        status: assetRes.ok ? 200 : assetRes.status,
-        headers,
-      });
+      // // ヘッダーとボディだけ返す
+      // return new Response(assetRes.body, {
+      //   status: assetRes.ok ? 200 : assetRes.status,
+      //   headers,
+      // });
     }
 
     // デフォルトは静的アセット返す
     const res = await env.ASSETS.fetch(request);
 
-    // // リクエストされたURLが存在しない場合は404ページを返す
-    if (res.status === 404) {
-      // 404.html を Assets から取得
-      const notFoundRes = await env.ASSETS.fetch(
-        new Request(new URL("/404.html", request.url), request)
-      );
+    // リクエストされたURLが存在しない場合は404ページを返す
+    // if (res.status === 404) {
+    //   // 404.html を Assets から取得
+    //   const notFoundRes = await env.ASSETS.fetch(
+    //     new Request(new URL("/404.html", request.url), request)
+    //   );
 
-      // 404.html があればそれを返す
-      if (notFoundRes.ok) {
-        return new Response(notFoundRes.body, {
-          status: 404,
-          headers: notFoundRes.headers,
-        });
-      }
+    //   // 404.html があればそれを返す
+    //   if (notFoundRes.ok) {
+    //     return new Response(notFoundRes.body, {
+    //       status: 404,
+    //       headers: notFoundRes.headers,
+    //     });
+    //   }
 
-      // 404.html も存在しない場合はシンプルな404メッセージ
-      return new Response("404 Not Found", { status: 404 });
-    }
+    //   // 404.html も存在しない場合はシンプルな404メッセージ
+    //   return new Response("404 Not Found", { status: 404 });
+    // }
 
     return res;
   },
